bde125bf239b4a337d94f9eb797d6ada7dd5d48e
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-05-11 09:42:02 +0900

    add block generator

---
+++ Assets/Scripts/TargetBlockGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ターゲットブロックを並べる
+/// 適当な GameObject に追加して使う
+/// </summary>
+public class TargetBlockGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject[] m_targetPrefabArray;
+    [SerializeField] Vector2 m_startGeneratePostion;
+    [SerializeField] int m_columns = 7;
+    [SerializeField] int m_rows = 6;
+
+    void Start()
+    {
+        Vector2 pos = m_startGeneratePostion;
+        float blockHeight = 0f, blockWidth = 0f;
+        for (int j = 0; j < m_rows; j++)
+        {
+            for (int i = 0; i < m_columns; i++)
+            {
+                int prefabIndex = j % m_targetPrefabArray.Length;
+                GameObject go = Instantiate(m_targetPrefabArray[prefabIndex], this.gameObject.transform);
+                go.transform.position = pos;
+                blockWidth = go.GetComponent<SpriteRenderer>().bounds.size.x;
+                blockHeight = go.GetComponent<SpriteRenderer>().bounds.size.y;
+                pos = pos + new Vector2(blockWidth, 0);
+            }
+            pos = new Vector2(m_startGeneratePostion.x, pos.y - blockHeight);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
