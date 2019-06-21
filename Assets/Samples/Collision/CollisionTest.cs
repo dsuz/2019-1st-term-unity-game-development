74f195f798d56e2e08cd6ca4fca2b945851b55a8
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-06-21 11:03:29 +0900

    フォローアップ用のシーン等を追加した

---
+++ Assets/Samples/Collision/CollisionTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 衝突判定をテストするためのスクリプト
+/// </summary>
+public class CollisionTest : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;    // ぶつかった「相手」の GameObject を取得する
+        string othersName = other.name; // ぶつかった相手の GameObject 名を取得する
+        Debug.Log(othersName + " にぶつかった");
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;    // ぶつかった「相手」の GameObject を取得する
+        string othersName = other.name; // ぶつかった相手の GameObject 名を取得する
+        Debug.Log(othersName + " に接触している");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        GameObject other = collision.gameObject;    // ぶつかった「相手」の GameObject を取得する
+        string othersName = other.name; // ぶつかった相手の GameObject 名を取得する
+        Debug.Log(othersName + " から離れた");
+    }
+}
---
+++ Assets/Samples/Velocity/VelocityTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rigidbody2D の使い方をテストするスクリプト
+/// </summary>
+public class VelocityTest : MonoBehaviour
+{
+    public void Accel()
+    {
+        Debug.Log("Accel() が呼ばれた");
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();   // 自分自身の Rigidbody2D コンポーネントを取ってくる
+        rb.velocity = rb.velocity * 1.5f;   // 速度ベクトルの大きさを 1.5 倍にする
+    }
+
+    public void HitUp()
+    {
+        Debug.Log("HitUp() が呼ばれた");
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();   // 自分自身の Rigidbody2D コンポーネントを取ってくる
+        rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);  // 上向きの力を加える
+    }
+}
